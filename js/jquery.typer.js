1235f4281b51bf0636edab89d7fa40aafeb57591
G_R <45249429+Ghost-Rider10@users.noreply.github.com>
2019-12-31 13:34:17 +0530

    Delete jquery.typer.js

--- js/jquery.typer.js
+++
@@ -1,5 +0,0 @@
-(function($){$.fn.typer=function(text,options){options=$.extend({},{char:'',delay:5000,duration:800,endless:!0,onType:$.noop,afterAll:$.noop,afterPhrase:$.noop},options||text);text=$.isPlainObject(text)?options.text:text;text=$.isArray(text)?text:text.split(" ");return this.each(function(){var elem=$(this),isVal={input:1,textarea:1}[this.tagName.toLowerCase()],isTag=!1,timer,c=0;(function typetext(i){var e=({string:1,number:1}[typeof text]?text:text[i])+'',char=e.substr(c++,1);if(char==='<'){isTag=!0}
-if(char==='>'){isTag=!1}
-elem[isVal?"val":"html"](e.substr(0,c)+($.isFunction(options.char)?options.char():options.char||' '));if(c<=e.length){if(isTag){typetext(i)}else{timer=setTimeout(typetext,options.duration/10,i)}
-options.onType(timer)}else{c=0;i++;if(i===text.length&&!options.endless){return}else if(i===text.length){i=0}
-timer=setTimeout(typetext,options.delay,i);if(i===text.length-1)options.afterAll(timer);options.afterPhrase(timer)}})(0)})}}(jQuery))
